213bf0ad9bf78c2fcd55182e02be16abeab009b2
mrfreddie04 <58453957+mrfreddie04@users.noreply.github.com>
2023-10-26 19:17:42 +0200

    add image support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
+import sword from "../images/swc-sword.png";
+import swordSvg from "../images/sword.svg";
 
 function App() {
   return (
     <>
-      <section className='hero'>Hero</section>
+      <section className='hero'></section>
       <main>
         <section>
           <h1>Hello from Rect App</h1>
         </section>
+        <img src={sword} alt='sword' width={250} />
+        <img src={swordSvg} alt='sword svg' width={250} />
+        <Recipes />
       </main>
-      <Recipes />
     </>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,37 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 let mode = process.env.NODE_ENV || "development";
-let target = mode === "production" ? "browserslis" : "web";
+let target = mode === "production" ? "browserslist" : "web";
 
 module.exports = {
   mode: mode,
   target: target,
+  output: {
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
 
   module: {
     rules: [
       {
+        test: /\.(svg|png|jpg|jpeg|gif)$/i,
+        type: "asset",
+        // parser: {
+        //   dataUrlCondition: {
+        //     maxSize: 30 * 1024,
+        //   },
+        // },
+      },
+      {
         test: /\.(s[ca]|c)ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" },
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
 
       {
